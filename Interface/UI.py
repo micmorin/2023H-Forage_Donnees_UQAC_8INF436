1988d455d16e3fd005a7afae43a2a8cd829c397d
Michael Munger <michael.munger1@uqac.ca>
2023-04-09 11:23:17 -0400

    Add initial Flask

--- Interface/UI.py
+++ Interface/UI.py
@@ -1,2 +1,24 @@
+from flask import Flask, request, render_template, url_for,redirect, flash
+
 def interfaceUtilisateur(modele1, modele2, modele3, donees):
-  pass
+  # Begin Flask App
+    app = Flask(__name__)
+
+    # Index.html
+    @app.route("/", methods=['GET', 'POST'])
+    def index():
+        # If GET, we return index.html
+        if request.method == 'GET': 
+            return render_template('index.html')
+        
+        # If POST, the team is selected so redirect to level 1
+        else:
+            return redirect(url_for('metrics'))
+
+    # Metrics.html
+    @app.route("/metrics", methods=['GET'])        
+    def metrics(index):
+        return render_template('index.html')
+
+    # Begin Flask Server
+    app.run(host='127.0.0.1',port=80)
