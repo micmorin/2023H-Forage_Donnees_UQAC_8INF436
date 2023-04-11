10c282c41aabf9f1aa41791af3b9a6c8036e6087
Michael Munger <michael.munger1@uqac.ca>
2023-04-11 10:29:45 -0400

    Rename prepdim to reducedim

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 from Classification.models import classificationEnTroisModele
-from Dimensions.dimensions import preparationDeDimension
+from Dimensions.dimensions import reductionDeDimension
 from Interface.UI import interfaceUtilisateur
 from Preparation.donnees import preparationDesDonnees
 
 if __name__ == "__main__":
   donnees =  preparationDesDonnees()
-  donnees = preparationDeDimension(donnees)
+  donnees = reductionDeDimension(donnees)
   modele1, modele2, modele3 = classificationEnTroisModele(donnees)
   interfaceUtilisateur(modele1, modele2, modele3, donnees)
